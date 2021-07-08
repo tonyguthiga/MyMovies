c1f83055f0dfa3eacc54525fed1ae47621c01f53
tonyguthiga <toniguthiga93@gmail.com>
2021-07-08 16:59:46 +0300

    add adapter and listview

--- app/src/main/java/com/moringaschool/mymovies/MoviesActivity.java
+++ app/src/main/java/com/moringaschool/mymovies/MoviesActivity.java
@@ -4,16 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MoviesActivity extends AppCompatActivity {
     private TextView mLocationTextView;
+    private ListView mListView;
+
+    private String[] movies = new String[] {"The Avengers", "The Big Bang Theory",
+            "Life of Pie", "The Bourne Identity", "The Martian", "The Originals",
+            "Sleepy Hollow", "Revolution", "The Rookie", "The Blacklist",
+            "The Vampire Diary", " Merlin", "Blackish",
+            "Game of Thrones", "Vikings"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
+
+        mListView = (ListView) findViewById(R.id.listView);
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, movies);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(MoviesActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the movies with the title: " + location);
